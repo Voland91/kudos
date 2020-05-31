ed1d74858fb705fee1b4831e608303543693fdbc
Mateusz Butkiewicz <butkiewicz.mat@gmail.com>
2020-05-31 10:13:18 +0200

    added hashtags to editor

--- src/components/atoms/TextInput.js
+++ src/components/atoms/TextInput.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import styled from 'styled-components';
 import { EditorState } from 'draft-js';
 import Editor from 'draft-js-plugins-editor';
 import createEmojiPlugin from 'draft-js-emoji-plugin';
-import styled from 'styled-components';
+import createHashtagPlugin from 'draft-js-hashtag-plugin';
 import createMentionPlugin, { defaultSuggestionsFilter } from 'draft-js-mention-plugin';
 import emojiEditorStyles from './editorStyles/emojiEditorStyles.css';
 import mentionsEditorStyles from './editorStyles/mentionsEditorStyles.css';
+import hashtagEditorStyles from './editorStyles/hashtagEditorStyle.css';
 
 const StyledEditorWrapper = styled.div`
   background-color: transparent;
@@ -31,6 +33,7 @@ class TextInput extends React.Component {
 
     this.mentionPlugin = createMentionPlugin();
     this.emojiPlugin = createEmojiPlugin();
+    this.hashtagPlugin = createHashtagPlugin();
   }
 
   state = {
@@ -54,11 +57,15 @@ class TextInput extends React.Component {
     const { editorState, suggestions } = this.state;
     const { MentionSuggestions } = this.mentionPlugin;
     const { EmojiSuggestions, EmojiSelect } = this.emojiPlugin;
-    const plugins = [this.mentionPlugin, this.emojiPlugin];
+    const plugins = [this.mentionPlugin, this.emojiPlugin, this.hashtagPlugin];
 
     return (
       <>
-        <StyledEditorWrapper className={(emojiEditorStyles.editor, mentionsEditorStyles.editor)}>
+        <StyledEditorWrapper
+          className={
+            (emojiEditorStyles.editor, mentionsEditorStyles.editor, hashtagEditorStyles.editor)
+          }
+        >
           <Editor editorState={editorState} onChange={this.onChange} plugins={plugins} />
           <MentionSuggestions onSearchChange={this.onSearchChange} suggestions={suggestions} />
           <EmojiSuggestions />
