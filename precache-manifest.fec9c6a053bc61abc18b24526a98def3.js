890fdeb68d48047554b144999651b7af68ec0afb
Mateusz Butkiewicz <butkiewicz.mat@gmail.com>
2020-06-02 13:22:00 +0200

    Updates

---
+++ precache-manifest.fec9c6a053bc61abc18b24526a98def3.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "6622df593dc5f15fcc15",
+    "url": "/kudos/static/css/main.3bd0cb4e.chunk.css"
+  },
+  {
+    "revision": "6622df593dc5f15fcc15",
+    "url": "/kudos/static/js/main.4223a773.chunk.js"
+  },
+  {
+    "revision": "1c8609e154559289f47e",
+    "url": "/kudos/static/js/runtime~main.7e66e30b.js"
+  },
+  {
+    "revision": "3fb3c0201fdf9d413b95",
+    "url": "/kudos/static/js/2.d9e01ce2.chunk.js"
+  },
+  {
+    "revision": "cde695c4923ef84cf409280121a2c331",
+    "url": "/kudos/static/media/agreement.cde695c4.svg"
+  },
+  {
+    "revision": "c43ce78932d794e3fe798c8bb3fa0228",
+    "url": "/kudos/static/media/sticky-note-regular.c43ce789.svg"
+  },
+  {
+    "revision": "5a184a47e7077715ae425d262f4562ea",
+    "url": "/kudos/static/media/smile-regular.5a184a47.svg"
+  },
+  {
+    "revision": "08900ddd7c914e72cd08ef60eb2459d7",
+    "url": "/kudos/static/media/paperclip-solid.08900ddd.svg"
+  },
+  {
+    "revision": "d5ba9fd843fa45207e90debe6b1212d8",
+    "url": "/kudos/static/media/heart-solid.d5ba9fd8.svg"
+  },
+  {
+    "revision": "729ac9c3d72de063794718954c5f0c94",
+    "url": "/kudos/static/media/comment-alt-solid.729ac9c3.svg"
+  },
+  {
+    "revision": "09e0a6e3e0ba51533829abaaf216118a",
+    "url": "/kudos/static/media/ellipsis-v-solid.09e0a6e3.svg"
+  },
+  {
+    "revision": "c119efe0bbf5e5f5c314b8b654661c8c",
+    "url": "/kudos/static/media/agreement_form.c119efe0.svg"
+  },
+  {
+    "revision": "18722765ed22b684ba1855363b062f60",
+    "url": "/kudos/static/media/close.18722765.svg"
+  },
+  {
+    "revision": "6def68d68970a0a4413f025f496eff71",
+    "url": "/kudos/index.html"
+  }
+];
